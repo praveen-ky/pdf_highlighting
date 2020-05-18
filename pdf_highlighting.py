05470f8c2eedf0c6239a0a5fd3da597cf291dee8
Praveen Kumar Yadav <63177137+praveen-ky@users.noreply.github.com>
2020-05-18 23:27:51 +0530

    Update pdf_highlighting.py

--- pdf_highlighting.py
+++ pdf_highlighting.py
@@ -1,29 +1,31 @@
 import fitz
 import sys
 import pandas as pd
-Excel_name = input("Enter the name of the Excel Sheet :")
-Sheet_name = input("Enter the Sheet Name :")
-df = pd.read_excel(Excel_name,sheet_name=Sheet_name)
 
-# Storing in a list format by taking a column
-tcs = df['EMPLOYEE NAME'].tolist()
-print(tcs)
+def excel():
+    Excel_name = input("Enter the name of the Excel Sheet :")
+    Sheet_name = input("Enter the Sheet Name :")
+    df = pd.read_excel(Excel_name,sheet_name=Sheet_name)
 
-print("Total no. of Elements in the Columns:",len(tcs))
+    # Storing in a list format by taking a column
+    tcs = df['EMPLOYEE NAME'].tolist()
+    print(tcs)
 
-# Opening the Pdf File and storing the Data in doc
-pdf_file = input("Enter the name of the PDF File: ")
-doc = fitz.open(pdf_file)
+    print("Total no. of Elements in the Columns:",len(tcs))
 
-for count in range(len(doc)):
-    multi_instances=[]
-    page = doc[count]
+    # Opening the Pdf File and storing the Data in doc
+    pdf_file = input("Enter the name of the PDF File: ")
+    doc = fitz.open(pdf_file)
 
-    for person in range(len(tcs)):         
-        quads = page.searchFor(str(tcs[person]), hit_max=100, quads=True)
-        
-        for instance in quads:
-            highlight = page.addHighlightAnnot(instance)
+    for count in range(len(doc)):
+        multi_instances=[]
+        page = doc[count]
 
-#Saving the File
-doc.save("Output1final.pdf",garbage=4, deflate=True, clean=True)
+        for person in range(len(tcs)):         
+            quads = page.searchFor(str(tcs[person]), hit_max=100, quads=True)
+
+            for instance in quads:
+                highlight = page.addHighlightAnnot(instance)
+
+    #Saving the File
+    doc.save("Output1final.pdf",garbage=4, deflate=True, clean=True)
